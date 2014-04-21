09e1c86a0e7b42bef94fee3627bbf1c4495f6778
Adam <adam@nullriver.com>
2014-04-21 19:31:34 -0400

    Added payment transaction observer stuff

---
+++ BTC Average/BAPaymentTransactionObserver.h
@@ -0,0 +1,14 @@
+//
+//  BAPaymentTransactionObserver.h
+//  ฿ Average
+//
+//  Created by Adam Dann on 2014-04-20.
+//  Copyright (c) 2014 Nullriver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <StoreKit/StoreKit.h>
+
+@interface BAPaymentTransactionObserver : NSObject <SKPaymentTransactionObserver>
+
+@end
