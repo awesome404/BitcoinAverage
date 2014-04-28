4326f5c006b4b43be5ce80e809af369f24725bec
Adam <adam@nullriver.com>
2014-04-28 17:47:29 -0400

    Fetching data for the graph works
    Needs to be cleaned up

--- BTC Average/BAGraphViewController.m
+++ BTC Average/BAGraphViewController.m
@@ -7,36 +7,115 @@
 //
 
 #import "BAGraphViewController.h"
+#import "BACurrency.h"
 
-@interface BAGraphViewController ()
+@interface BAGraphViewController () {
+    NSNumber *highPrice,
+             *lowPrice,
+             *startTime,
+             *endTime;
+    NSArray *theData;
+}
+
+@property NSString *currency;
+
+- (void)refreshData;
 
 @end
 
 @implementation BAGraphViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+/*- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
     }
     return self;
-}
+}*/
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-/*- (void)viewWillAppear:(BOOL)animated {
-}*/
+- (void)viewWillAppear:(BOOL)animated {
+    [self refreshData];
+}
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)refreshData {
+    static NSString *urlFormat = @"https://api.bitcoinaverage.com/history/%@/per_minute_24h_sliding_window.csv";
+
+    self.currency = [BACurrency get];
+    NSStringEncoding *encoding=nil;
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:urlFormat,self.currency]];
+    NSString *urlData = [NSString stringWithContentsOfURL:url usedEncoding:encoding error:nil];
+    
+    NSMutableArray *mutableData = [NSMutableArray array];
+
+    if(urlData) {
+        NSArray *lines = [urlData componentsSeparatedByString:@"\n"];
+        urlData = nil;
+
+        BOOL firstLine = YES;
+        NSRange range;
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]];
+        [dateFormatter setDateFormat:@"y-M-d HH:mm:ss"];
+        NSNumber *average = nil;
+        
+        highPrice = nil;
+        lowPrice = nil;
+
+        for(NSString *line in lines) {
+            if(firstLine) firstLine = NO;
+            else if([line length]>0) {
+                
+                range = [line rangeOfString:@","];
+                average = [NSNumber numberWithDouble:[[line substringFromIndex:(range.location+range.length)] doubleValue]];
+                
+                if(highPrice == nil || average>highPrice) highPrice = average;
+                if(lowPrice == nil || average<lowPrice) lowPrice = average;
+
+                [mutableData addObject:
+                    [NSArray arrayWithObjects:
+                        [NSNumber numberWithDouble:[[dateFormatter dateFromString:[line substringToIndex:range.location]] timeIntervalSince1970]],
+                        average, //[NSNumber numberWithDouble:[[line substringFromIndex:(range.location+range.length)] doubleValue]],
+                        nil
+                     ]
+                ];
+
+            }
+        }
+        
+        theData = [NSArray arrayWithArray:mutableData];
+        
+        startTime = theData[0][0];
+        endTime = theData[[theData count]-1][0];
+
+    } else NSLogDebug(@"No urlData",nil);
+}
+
+/*
+ 
+ - (NSString*)reformatTimestamp:(NSString*)stamp {
+ 
+ NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+ [dateFormatter setDateFormat:@"EEE, dd LLL yyyy HH:mm:ss ZZZ"];
+ NSDate *theDate = [dateFormatter dateFromString:stamp];
+ 
+ [dateFormatter setTimeStyle:NSDateFormatterMediumStyle];
+ [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+ [dateFormatter setLocale:[NSLocale currentLocale]];
+ 
+ return [dateFormatter stringFromDate:theDate];
+ }
+ 
+*/
 
 /*
 #pragma mark - Navigation
